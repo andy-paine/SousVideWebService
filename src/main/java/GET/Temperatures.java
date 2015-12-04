d2dd0d3e69dee304dc48d7a3d8e38855b52915ef
Andrew  Paine <andrew.paine14@imperial.ac.uk>
2015-12-04 01:26:33 +0000

    Added new endpoint

--- src/main/java/GET/Temperatures.java
+++ src/main/java/GET/Temperatures.java
@@ -1,10 +1,12 @@
 package GET;
 
 import Util.IDataStore;
+import Util.Temperature;
 import com.google.gson.Gson;
 import org.restlet.ext.json.JsonRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.resource.Get;
+import org.restlet.resource.Put;
 import org.restlet.resource.ResourceException;
 import org.restlet.resource.ServerResource;
 
@@ -26,5 +28,11 @@ public class Temperatures extends ServerResource {
     public Representation getTemperatures() {
         String json = new Gson().toJson(dataStore.getTemperatures());
         return new JsonRepresentation(json);
+    }
+
+    @Put("json")
+    public void logTemperature(String json) {
+        Temperature temp = new Gson().fromJson(json, Temperature.class);
+        dataStore.logTemperature(temp);
     }
 }
